cd4f53203fb268c3af1f36708e326f2e916c29e9
Alexander Semenov <modern.rising@outlook.com>
2021-05-11 13:40:32 +0300

    fix 1.2

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
 
-const toggleActive = function () {
-  let link = document.querySelectorAll(".menu__link");
-  for (var i = 0; i < link.length; i++) {
-    link[i].addEventListener("click", function() {
-      let current = document.getElementsByClassName("menu__link_active");
-      current[0].className = current[0].className.replace(" menu__link_active", "");
-      this.className += " menu__link_active";
-    });
-  }
-};
-
-toggleActive();
-
-
 window.addEventListener("DOMContentLoaded", () => {
+  const toggleActive = function () {
+    let link = document.querySelectorAll(".menu__link");
+    for (var i = 0; i < link.length; i++) {
+      link[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("menu__link_active");
+        current[0].className = current[0].className.replace(" menu__link_active", "");
+        this.className += " menu__link_active";
+      });
+    }
+  };
+  
+  toggleActive();
+
+  
   const hamburger = document.querySelector(".c-hamburger");
   const menu = document.querySelector(".menu");
   const menuItem = menu.querySelectorAll(".menu__item");
@@ -30,3 +30,10 @@ window.addEventListener("DOMContentLoaded", () => {
   });
   });
 });
+
+jQuery(document).ready(function($){
+  function scrollToElement (id){
+    $('html, body').animate({scrollTop: $('#' + id).offset().top},);
+  }
+});
+
